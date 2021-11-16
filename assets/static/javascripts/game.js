bbf10ae880a0381b8fc23d327ee9a7d9028e13c2
Tavares913 <jakeknocker74@gmail.com>
2021-11-15 21:30:41 -0500

    Reeeeealy simple example of the letter a falling down and clicking a button to stop it

--- assets/static/javascripts/game.js
+++ assets/static/javascripts/game.js
@@ -1,7 +1,61 @@
+// Create a p and configure it
 const p = document.createElement('p');
-p.innerText = "This second line is a test, it was added via javascript";
-console.log(p);
+p.innerText = "A";
+p.style.position = "absolute";
+p.style.top = "50px";
+
+// Add an event listener to the a button
+const button = document.querySelector('button.a-key');
+button.addEventListener('click', clickA);
+
+// Append it to h1
 const h1 = document.querySelector('h1');
-console.log(h1);
 h1.append(p);
-console.log("here");
+
+// Slide it down every 10 milliseconds
+const pIntervalId = setInterval(() =>
+{
+    slideDown(p);
+}, 5);
+
+// Just made these for sliding down
+function slideDown(elem)
+{
+    let curYStr = elem.style.top;
+    let curY = pixelVal(curYStr);
+    curY += 1;
+    if (curY >= 700)
+    {
+        elem.remove();
+        clearInterval(pIntervalId);
+    }
+    elem.style.top = `${curY}px`;
+}
+function pixelVal(curYStr)
+{
+    if (typeof parseInt(curYStr.substring(0, 4) == 'number'))
+    {
+        return parseInt(curYStr.substring(0, 4));
+    }
+    else if (typeof parseInt(curYStr.substring(0, 3) == 'number'))
+    {
+        return parseInt(curYStr.substring(0, 3));
+    }
+    else if (typeof parseInt(curYStr.substring(0, 2) == 'number'))
+    {
+        return parseInt(curYStr.substring(0, 2));
+    }
+    else
+    {
+        return parseInt(curYStr.substring(0, 1));
+    }
+}
+
+// Click a function
+function clickA()
+{
+    if (450 <= pixelVal(p.style.top))
+    {
+        p.remove();
+    }
+}
